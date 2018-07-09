4a5574e859073a1eb838f19cda08516f069f87c9
archana.kl <archana.kl@sonata-software.com>
2018-07-09 17:40:00 +0530

    Code

--- Components/utility/commonJs.js
+++ Components/utility/commonJs.js
@@ -1,3 +1,7 @@
+ /**
+  * Function : checkForUndefinedORNull
+  * Description : Function checks for undefined or null or w=empty value of data and returns true if data satisfied with any of these.
+  */
 export function checkForUndefinedORNull(data) {
     return (data == undefined ||
     data == "undefined" ||
